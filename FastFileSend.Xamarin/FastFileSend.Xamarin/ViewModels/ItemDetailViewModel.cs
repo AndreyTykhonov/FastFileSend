72028a8a708de2ef576bc4e82ec85fd06c4ef42a
Andrew Andrew <andreytykhonov96@gmail.com>
2019-12-08 11:28:10 +0200

    Xamarin Android

---
+++ FastFileSend.Xamarin/FastFileSend.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FastFileSend.Xamarin.Models;
+
+namespace FastFileSend.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
