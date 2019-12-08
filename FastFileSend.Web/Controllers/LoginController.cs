d79c716f8716f13eb5a99fe9de1e733d9c0d32b6
Andrew Andrew <andreytykhonov96@gmail.com>
2019-12-08 03:34:14 +0200

    Login temp API

---
+++ FastFileSend.Web/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace FastFileSend.Web.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // GET: Login
+        public bool Get(int id)
+        {
+            return true;
+        }
+    }
+}
