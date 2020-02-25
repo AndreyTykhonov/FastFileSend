b86b4b5f885321548c60956b2f05fe53adba3934
Andrew Tykhonov <andreytykhonov96@gmail.com>
2020-02-25 13:02:02 +0200

    Asp.Core server base

---
+++ FastFileSend.WebCore/AuthOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFileSend.WebCore
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "FastFileSend.Server"; // издатель токена
+        public const string AUDIENCE = "FastFileSend.Client"; // потребитель токена
+        const string KEY = "TransparentElephant";   // ключ для шифрации
+        public const int LIFETIME = 24 * 60; // время жизни токена - 1 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
