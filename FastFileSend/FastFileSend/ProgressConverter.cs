d805f88eb3bb9cb976daa1e55c9ffb21ffb56cfd
Andrew Tykhonov <AndreyTykhonov96@gmail.com>
2020-03-11 06:37:31 +0200

    ProgressConverter Android

---
+++ FastFileSend/FastFileSend/ProgressConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FastFileSend
+{
+    class ProgressConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value * 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double) 0;
+        }
+    }
+}
