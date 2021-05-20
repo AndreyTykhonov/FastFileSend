9750c5705f22e637c48cf491417d415f8f727e6d
Andrey Tykhonov <andreytykhonov96@gmail.com>
2021-05-20 20:26:33 +0300

    Nuget updates

--- FastFileSend/FastFileSend/FastFileSendPlatformDialogsXamarin.cs
+++ FastFileSend/FastFileSend/FastFileSendPlatformDialogsXamarin.cs
@@ -2,12 +2,9 @@
 using FastFileSend.Main.Models;
 using FastFileSend.Main.ViewModel;
 using FastFileSend.Views;
-using Plugin.FilePicker;
-using Plugin.FilePicker.Abstractions;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace FastFileSend
@@ -16,18 +13,16 @@ namespace FastFileSend
     {
         public async Task<FileInfo> SelectFileAsync()
         {
-            FileData fileData = await CrossFilePicker.Current.PickFile();       
+            FileResult fileData = await FilePicker.PickAsync();
 
             if (fileData == null)
                 return null;
 
-            System.IO.MemoryStream stream = new System.IO.MemoryStream(fileData.DataArray);
-
             FileInfo fileInfo = new FileInfo
             {
                 Name = System.IO.Path.GetFileName(fileData.FileName),
-                Content = stream,
-            };
+                Content = await fileData.OpenReadAsync(),
+        };
 
             return fileInfo;
         }
